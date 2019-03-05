5705b1832d28d9dfe25aad7c6f09ecce66b77f24
grvlle <gralle89@gmail.com>
2019-03-05 16:36:38 +0100

    Setting up event listening channels

--- qbot.go
+++ qbot.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 
 	"github.com/nlopes/slack"
@@ -21,9 +22,15 @@ type qBot struct {
 	rtm   *slack.RTM
 	Users map[string]slack.User
 	//Channels map[string]Channel
+
+	//Listeners
+	Listener chan *Listener
 }
 
 func (qb *qBot) LoadConfig() *qBot {
+
+	qb.Listener = make(chan *Listener, 500)
+
 	content, err := ioutil.ReadFile("config.yaml")
 	err = yaml.Unmarshal(content, &qb.Config)
 	if err != nil {
@@ -43,14 +50,44 @@ func (qb *qBot) RunBot() {
 }
 
 func (qb *qBot) SetupHandlers() {
-	return
+	go qb.EventListener(qb.Listener)
+}
+
+func (qb *qBot) EventListener(listen *Listener) {
+
+	for msg := range qb.rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+
+		case *slack.MessageEvent:
+			msg := ev.Text
+			fmt.Println(msg)
+			listen <- msg
+		case *slack.ConnectedEvent:
+			fmt.Println("Infos:", ev.Info)
+			fmt.Println("Connection counter:", ev.ConnectionCount)
+			// Replace C2147483705 with your Channel ID
+			//qb.rtm.SendMessage(qb.rtm.NewOutgoingMessage("Hello world", "C2147483705"))
+
+		case *slack.PresenceChangeEvent:
+			fmt.Printf("Presence Change: %v\n", ev)
+
+		case *slack.LatencyReport:
+			fmt.Printf("Current latency: %v\n", ev.Value)
+
+		case *slack.RTMError:
+			fmt.Printf("Error: %s\n", ev.Error())
+
+		case *slack.InvalidAuthEvent:
+			fmt.Printf("Invalid credentials")
+			return
+		}
+	}
 }
 
 func main() {
 	var qb qBot
 	qb.LoadConfig()
 	qb.RunBot()
-
 	//fmt.Print(SlackConfig.APIToken)
 
 }
